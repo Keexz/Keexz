cf2e17169e458e444ea694576fcf9e26eec5be7d
Keexz <ayekadaniel7@gmail.com>
2025-03-26 22:22:29 +0100

    Worked on the calculation.py

--- calculate.py
+++ calculate.py
@@ -1,20 +1,12 @@
-import time
 import typer
 from speechengine import speechEngine
-import numpy
 
-def calculate():
-    prompt = "calculate 1 + 20 + 5".split()
-    
-    no = []
-    
-    with typer.open_file('numbers.txt', encoding='utf-8-sig') as nums:
-        num = nums.read()
-        for i in prompt:
-            if i in num:
-                    no.append(int(i))
-        
-        if "+" in prompt:
-            typer.echo(numpy.sum(no))
 
-calculate()
+def calculate(prompt:list):
+    
+    p = " ".join(prompt)
+    NewPrompt = p.replace("calculate", "")
+    ans = f"The result of the calculation is {eval(NewPrompt)}"
+    typer.echo(message=f"\nSARA: {ans}\n")
+    speechEngine(ans)
+    
